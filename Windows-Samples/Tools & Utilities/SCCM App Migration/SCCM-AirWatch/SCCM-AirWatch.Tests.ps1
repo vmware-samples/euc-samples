6dbbb223f078fe29a9b7e804b8ae6aca5ef5e85f
Mike Nelson <miken@vmware.com>
2017-09-19 13:55:51 -0400

    Add unit tests to refactored module code Signed-off-by: Mike Nelson <miken@vmware.com>

---
+++ Windows-Samples/Tools & Utilities/SCCM App Migration/SCCM-AirWatch/SCCM-AirWatch.Tests.ps1
@@ -0,0 +1,20 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "SCCM XML Extraction" {
+    It "processes Adobe sample" {
+        # Given
+        $adobe = [xml](Get-Content .\SCCM-AirWatch\SampleXML\AdobeStandard.xml)
+        
+        # When
+        $properties = Extract-PackageProperties -SDMPackageXML $adobe
+
+        # Then
+        $properties.ApplicationName | Should be "Acrobat Standard 11.0.20"
+        
+        #$current = $adobe.AppMgmtDigest.DeploymentType | Select-Object -First 1
+        #$properties.UploadFilePath | Should be "C:\tmp\Adobe\Acrobat\Standard\11.0.20\"
+        
+    }
+}
