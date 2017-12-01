2a982b0efd1c255785f67c914d13ec5a2a004927
Josue Negron <jnegron@vmware.com>
2017-12-01 08:53:15 -0600

    Added SCCM SQL Query for Batch Import into AirWatch

---
+++ Windows-Samples/Tools & Utilities/SCCM SQL Query for Batch Import/README.md
@@ -0,0 +1,28 @@
+# SCCM SQL Query for AirWatch Batch Import
+
+## Overview
+- **Author**: Brooks Peppin & Josue Negron
+- **Email**: jnegron@vmware.com
+- **Date Created**: 12/1/2017
+- **Tested on**: SCCM 1706 with SQL Server 2012
+
+## SYNOPSIS
+In order to silently onboard Windows 10 devices into AirWatch and have them automatically assigned to the primary user, you will have to first preregister devices via a batch import. You can use the attached SQL queries to generate the CSV file needed to perform a batch import in AirWatch. 
+
+Detailed instructions can be found at [http://www.brookspeppin.com](http://www.brookspeppin.com/blog/how-to-silently-enroll-windows-10-systems-into-airwatch-using-sccm)
+	
+## Modifications Required
+Update the following attributes before executing the script: 
+
+- **YourGroupID** - Group ID of your OG
+- **YourDeviceGroupID** - Enrollment OG for the Device
+- **YourCollectionID** - Device Collection ID for the collection you want to export
+
+        
+There are two version **Simple** and **Advanced**, all that is required is simple however some will want to add additional attributes ahead of time so they are imported with the device records. 
+
+## Resources
+- [Brooks Peppin Blog with How to Steps](http://www.brookspeppin.com/blog/how-to-silently-enroll-windows-10-systems-into-airwatch-using-sccm)
+
+
+
---
+++ Windows-Samples/Tools & Utilities/SCCM SQL Query for Batch Import/SCCMBatchImportSimple.sql
@@ -0,0 +1,29 @@
+select DISTINCT
+sys.User_Name0 as 'Username*',
+'' as 'Password',
+vru.givenName0 as 'First Name*',
+vru.sn0 as 'Last Name*',
+'YourGroupID' as 'GroupID*', 						-- Your Group ID here
+'Directory' as 'Security Type*',
+vru.User_Principal_Name0 as 'Email Address*',
+'' as 'User Role',
+'' as 'User Message Type',
+'YourGroupID' as 'Device GroupID', 					-- Your Group ID here
+'' as 'Device Friendly Name',
+v_GS_PC_BIOS.SerialNumber0 as 'Device Serial Number',
+'' as 'Device Platform'
+FROM v_R_System sys JOIN v_GS_PC_BIOS on  sys.ResourceID =  v_GS_PC_BIOS.ResourceID JOIN v_GS_COMPUTER_SYSTEM on sys.ResourceID = v_GS_COMPUTER_SYSTEM.ResourceID
+join v_FullCollectionMembership FCM on FCM.ResourceID = v_GS_COMPUTER_SYSTEM.ResourceID
+INNER JOIN
+     v_GS_COMPUTER_SYSTEM cs on sys.resourceID = cs.resourceID
+LEFT JOIN
+     V_GS_Operating_system os on sys.resourceID = os.resourceID
+LEFT JOIN
+     v_R_User vru ON sys.User_Name0 = vru.User_Name0
+Where FCM.CollectionID = 'YourCollectionID' 		-- Your Collection ID 
+and sys.User_Name0 != 'Null'  						-- Remove Null Usernames
+and sys.User_Name0 != 'Administrator' 				-- Remove Administrators
+and vru.User_Principal_Name0 != 'Null' 				-- Remove Null UPN
+and vru.givenName0 != 'Null' 						-- Remove Null First Name
+and vru.sn0 != 'Null' 								-- Remove Null Last Name
+and v_GS_PC_BIOS.SerialNumber0 not like '%vmware%' 	-- Optional, Remove VMs, Remove VMs with serial numbers starting with VMware
