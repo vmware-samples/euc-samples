787b26ecda89d346514004c7fdfaa83c5333aa55
Mike Nelson <miken@vmware.com>
2018-02-01 14:11:37 -0500

    Add SFD staging scripts and Readme Signed-off-by: Mike Nelson <miken@vmware.com>

---
+++ Windows-Samples/Tools & Utilities/Software Distribution Staging Scripts/Export-CachedAirWatchApplications.ps1
@@ -0,0 +1,39 @@
+﻿#Requires -RunasAdministrator
+
+Function Get-DestinationFolder {
+    Param(
+        [string]$Description="Select Export Destination Folder",
+        [string]$BaseDirectory="Desktop"
+    )
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+
+    $FolderDialog = New-Object System.Windows.Forms.FolderBrowserDialog
+    $FolderDialog.RootFolder = $BaseDirectory
+    $FolderDialog.Description = $Description
+    $FolderDialog.ShowDialog() | Out-Null
+    $FolderDialog.SelectedPath
+}
+
+Function Export-CachedAirWatchApplications {
+    
+    $DestinationPath = Get-DestinationFolder
+    $SourcePath = 'C:\ProgramData\AirWatchMDM\AppDeploymentCache'
+
+    if($(Test-Path $DestinationPath)) {
+        
+        Write-Host "Showing dialog to get Deployed Cache"
+        $CachedApps = Get-ChildItem -Path $SourcePath
+
+        Foreach($App in $CachedApps) {
+            try {
+                Write-Host "Compressing $($App.BaseName)"
+                Compress-Archive -Path $App.FullName -DestinationPath $DestinationPath\$($App.BaseName).zip
+            } catch {
+                Write-Host "Unable to compress $($App.Name)"
+            }
+        }   
+    } 
+}
+
+#Main
+Export-CachedAirWatchApplications
