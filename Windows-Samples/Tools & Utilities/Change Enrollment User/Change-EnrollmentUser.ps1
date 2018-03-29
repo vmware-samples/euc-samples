0e5067a1438a70790c14758465d43779b2d04271
Mike Nolterieke <mnolterieke@mnolterieke-a01.vmware.com>
2018-03-29 15:23:19 -0400

    Added ability to pass in single device serial number and user ID instead of requiring the csv file
    Signed-off-by: Mike Nolterieke <mnolterieke@mnolterieke-a01.vmware.com>

--- Windows-Samples/Tools & Utilities/Change Enrollment User/Change-EnrollmentUser.ps1
+++ Windows-Samples/Tools & Utilities/Change Enrollment User/Change-EnrollmentUser.ps1
@@ -12,16 +12,22 @@
 [CmdletBinding()]
 Param(
     [Parameter(Mandatory=$False)]
-    [string]$awServer = "https://mondecorp.ssdevrd.com",
+    [string]$awServer = "https://YourURL.com",
 
     [Parameter(Mandatory=$False)]
-    [string]$awTenantAPIKey = "iVvGQnSXpX3eliczZPaIlC8hCe5Q/kw22K3glhE+g/g=",
+    [string]$awTenantAPIKey = "YourAPIKey",
 
     [Parameter(Mandatory=$False)]
-    [string]$awAPIUsername = "mondecorp\tkent",
+    [string]$awAPIUsername = "YourUserName",
 
     [Parameter(Mandatory=$False)]
-    [string]$awAPIPassword = "Passw0rd1"
+    [string]$awAPIPassword = "YourPassword",
+
+    [Parameter(Mandatory=$False)]
+    [string]$serialNum,
+
+    [Parameter(Mandatory=$False)]
+    [string]$uName
 )
 
 <#
@@ -69,7 +75,7 @@ function Get-AirWatchVersion {
         [Parameter(Mandatory=$True)]
         [hashtable] $headers
     )
-    
+
     try {
         $endpoint = "$awServer/api/system/info"
 	    $response = Invoke-RestMethod -Method Get -Uri $endpoint.ToString() -Headers $headers
@@ -80,7 +86,7 @@ function Get-AirWatchVersion {
         $response = $_.Exception.Response | ConvertTo-Json
         Write-Verbose "Querying AirWatch version ($endpoint) Failed! Exception :: $($_.Exception.Message)"
         Write-Verbose "RESPONSE :: $($_.Exception.Response | ConvertTo-Json)"
-    } 
+    }
     catch {
         $response = $null
         Write-Verbose "Get AirWatch Version failed :: $PSItem"
@@ -202,7 +208,7 @@ Function Get-EnrollmentUserID {
     )
 
     $endpointURL = "$($awServer)/api/system/users/search?username=$($enrollmentUser)"
-        
+
     $response = Invoke-AirWatchAPIRequest -headers $headers -Verb GET -awURL $endpointURL
 
     #Search for user in returned Array
@@ -250,8 +256,7 @@ Function Change-EnrollmentUser
 
 
 Function Main {
-    
-    
+
     # Build API Headers
     $authString = Create-BasicAuthHeader -username $awAPIUsername -password $awAPIPassword
     $headers = Create-Headers -authString $authString -tenantCode $awTenantAPIKey -acceptType "application/json"
@@ -261,21 +266,34 @@ Function Main {
     $version = Get-AirWatchVersion -headers $headers
     Write-Host "AirWatch Version is $($version)"
 
-    # Fetch Mapping Info
-    Write-Host "Prompting User for Mapping CSV"
-    $csv = Get-CSVFilePath
-    $DeviceMapping = Import-Csv -Path $csv
+    # If they passed in the optional serial number and user name args
+    if ($serialNum -And $uName) {
+      # Manually create the custom object
+      # This is doing the same thing that Import-Csv does
+      $tempObject = New-Object PSCustomObject
+      $tempObject | Add-Member -type NoteProperty -name SerialNumber -Value $serialNum
+      $tempObject | Add-Member -type NoteProperty -name Username -Value $uName
+      # Manually create the DeviceMapping array with our object
+      $DeviceMapping = @($tempObject)
+    }
+    # They did not pass both args, prompt for the csv
+    else {
+      # Fetch Mapping Info
+      Write-Host "Prompting User for Mapping CSV"
+      $csv = Get-CSVFilePath
+      $DeviceMapping = Import-Csv -Path $csv
+    }
 
     Write-Host "Processing $($DeviceMapping.count) records"
 
     Foreach($Device in $DeviceMapping) {
         # Get Device ID
-        Write-Host "Fetching Device ID from AirWatch for $($Device.Username)"
+        Write-Host "Fetching Device ID from AirWatch for $($Device.SerialNumber)"
         $deviceID = Get-DeviceID -headers $headers -serialNumber $Device.SerialNumber
         Write-Host "Device ID is $($deviceID)"
-        
-        # Get Enrollment USer ID
-        Write-Host "Fetching Enrollment User ID from AirWatch"
+
+        # Get Enrollment User ID
+        Write-Host "Fetching Enrollment User ID from AirWatch for $($Device.UserName)"
         $userID = Get-EnrollmentUserID -headers $headers -enrollmentUser $Device.UserName
         Write-Host "User ID is $($userID)"
 
@@ -283,7 +301,7 @@ Function Main {
             # Make Switch
             Write-Host "Switching device to Enrollment user"
             $response = Change-EnrollmentUser -headers $headers -deviceID $deviceID -userID $userID
-            
+
             # Confirm Switch
             $serverUser = Verify-UserChange -headers $headers -serialNumber $Device.SerialNumber
 
@@ -293,11 +311,7 @@ Function Main {
                 Write-Host "Mapping failed for $($Device.UserName)" -ForegroundColor Red
             }
         }
-        
-        
-        
     }
-
 }
 
 Main
