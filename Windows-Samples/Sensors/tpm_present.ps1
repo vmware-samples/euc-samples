12638c011eaac5d3f2d9e5ab99438ab7514cdfc2
Andreano Lanusse <alanusse@vmware.com>
2019-04-29 16:10:15 -0400

    Revert "Revert "Merge branch 'master' of https://github.com/vmwaresamples/AirWatch-samples""
    This reverts commit 1f8859ca4fe0841876c5303d47ea75a4114cb24c.

--- Windows-Samples/Sensors/tpm_present.ps1
+++ Windows-Samples/Sensors/tpm_present.ps1
@@ -1,5 +1,5 @@
 # Returns True/False whether there is a TPM on the current computer
 # Return Type: Boolean
-# Execution Context: Admin
+# Execution Context: System
 $tpm=get-tpm
 write-output $tpm.TpmPresent
