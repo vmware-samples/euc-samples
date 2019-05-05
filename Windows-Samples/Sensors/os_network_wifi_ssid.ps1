81ce6303565e76fed1b87d8e7b459aa0e4958c08
Josue Negron <jnegron@vmware.com>
2019-05-04 23:58:09 -0500

    Updated os_network_wifi_ssid.ps1 to account for null values e.g. not connected to wifi
    Signed-off-by: Josue Negron <jnegron@vmware.com>

--- Windows-Samples/Sensors/os_network_wifi_ssid.ps1
+++ Windows-Samples/Sensors/os_network_wifi_ssid.ps1
@@ -1,5 +1,10 @@
 # Returns the currently connected SSID
 # Execution Context: User
+
 $ssid = netsh wlan show interfaces | select-string SSID
-$ssid = $ssid[0] | select-string  'SSID'
-Write-Output ($ssid -split ":")[-1].Trim()
+if ($ssid -ne $null) {
+  $ssid = $ssid[0] | select-string  ‘SSID’
+  Write-Output ($ssid -split “:”)[-1].Trim()
+} else {
+  Write-Output “”
+}
