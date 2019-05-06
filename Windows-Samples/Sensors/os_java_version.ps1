f07da45a6406575f97c6a23714c029cc0fee6f79
Josue Negron <jnegron@vmware.com>
2019-05-06 08:19:57 -0500

    Updated os_jave_version with error handling
    Signed-off-by: Josue Negron <jnegron@vmware.com>

--- Windows-Samples/Sensors/os_java_version.ps1
+++ Windows-Samples/Sensors/os_java_version.ps1
@@ -1,5 +1,15 @@
 # Returns Java Version e.g. 8.0.1910.12
 # Return Type: String
 # Execution Context: User
-$javaver=(Get-Command java | Select-Object -ExpandProperty Version).toString()
-write-output $javaver
+
+$javaver=(Get-Command java -ErrorVariable err -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Version)
+
+if ($err.Count -eq 0) {
+  $javaver = $javaver.ToString()
+}
+else
+{
+ $javaver = “JAVA not found”
+}
+
+write-output $javaver.ToString()
