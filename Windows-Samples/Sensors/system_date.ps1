1b9b5fe688461a9cdcb89130e3a873348f09dbfe
Andreano Lanusse <alanusse@vmware.com>
2019-07-17 01:55:50 -0400

    Update date time return to ISO format
    @andreano

--- Windows-Samples/Sensors/system_date.ps1
+++ Windows-Samples/Sensors/system_date.ps1
@@ -1,5 +1,5 @@
 # Returns the system's date and time
 # Return Type: DateTime
 # Execution Context: User
-$date_current = get-Date -DisplayHint Date
+$date_current = get-Date -format s -DisplayHint Date
 write-output $date_current
