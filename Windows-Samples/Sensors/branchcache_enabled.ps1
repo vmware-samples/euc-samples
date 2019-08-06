011077ff8cd3ec74be58f10d0e263b4687010bc9
Josue Negron <jnegron@vmware.com>
2019-08-06 11:29:43 -0500

    Added BranchCache Mode Sensor
    Signed-off-by: Josue Negron <jnegron@vmware.com>

--- Windows-Samples/Sensors/branchcache_enabled.ps1
+++ Windows-Samples/Sensors/branchcache_enabled.ps1
@@ -1,5 +1,5 @@
 # Returns true if BranchCache is Enabled
 # Return Type: Boolean
 # Execution Context: System
-$brachcache = Get-BCStatus
-write-output $brachcache.BranchCacheIsEnabled
+$branchcache = Get-BCStatus
+write-output $branchcache.BranchCacheIsEnabled
--- Windows-Samples/Sensors/branchcache_service_status.ps1
+++ Windows-Samples/Sensors/branchcache_service_status.ps1
@@ -1,5 +1,5 @@
 # Returns the Service state for the Branch Cache Service e.g. Stopped/Running
 # Return Type: String
 # Execution Context: System
-$brachcache = Get-BCStatus
-write-output $brachcache.BranchCacheServiceStatus
+$branchcache = Get-BCStatus
+write-output $branchcache.BranchCacheServiceStatus
