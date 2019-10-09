11e53facb8f75d72fe6cc9001c3ce18029032f48
brookspeppin <bpeppin@vmware.com>
2019-10-09 12:27:12 -0600

    Updated battery sensors
    Updated battery sensors to include detection if no battery is avail

--- Windows-Samples/Sensors/battery_max_capacity.ps1
+++
@@ -1,5 +0,0 @@
-# Returns the max charge capacity of the batteries
-# Return Type: Integer
-# Execution Context: System
-$max_capacity = (Get-WmiObject -Class "BatteryFullChargedCapacity" -Namespace "ROOT\WMI").FullChargedCapacity | Measure-Object -Sum | Select-Object -ExpandProperty Sum
-write-output $max_capacity
