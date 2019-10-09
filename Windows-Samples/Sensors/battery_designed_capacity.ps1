750165070f31fb388a76c38509a46d9ff4cb556b
brookspeppin <33036379+brookspeppin@users.noreply.github.com>
2019-10-09 11:52:41 -0600

    Revert "Update battery_designed_capacity.ps1"

--- Windows-Samples/Sensors/battery_designed_capacity.ps1
+++ Windows-Samples/Sensors/battery_designed_capacity.ps1
@@ -1,21 +1,5 @@
-﻿# Returns the max charge capacity of the batteries in percentage
-# Return Type: String
+﻿# Returns the max charge capacity of the batteries
+# Return Type: Integer
 # Execution Context: System
-# Author: Bpeppin
-
-# Check for existence of battery
-if ((Get-WmiObject -Class Win32_Battery).count -ne 0)
-{
-	
-	$designedCapacity = (Get-WmiObject -Class "BatteryStaticData" -Namespace "ROOT\WMI").DesignedCapacity
-	$fullCharge = (Get-WmiObject -Class "BatteryFullChargedCapacity" -Namespace "ROOT\WMI").FullChargedCapacity
-	# 
-	$currentBatteryCapacity = ($FullCharge / $DesignedCapacity) * 100
-	# Round battery percentage
-	$currentBatteryCapacity = [decimal]::round($currentBatteryCapacity)
-	Write-Host "Battery capacity is at $($currentBatteryCapacity)%."
-}
-else
-{
-	Write-Output "No battery found"
-}
+$design_capacity = (Get-WmiObject -Class “BatteryStaticData” -Namespace “ROOT\WMI”).DesignedCapacity | Measure-Object -Sum | Select-Object -ExpandProperty Sum
+write-output $design_capacity
