9d387d9fd8b7acb2bba8bd0de03318acf8729f60
brookspeppin <bpeppin@vmware.com>
2020-02-24 15:52:54 -0700

    Published to git

---
+++ Windows-Samples/Sensors/get_enrollment_sid_32_64.ps1
@@ -0,0 +1,72 @@
+﻿# bpeppin, Updated 2/24/20. 
+# Querying which Windows SID has MDM Enrollment
+# Return Type: String
+# Execution Context: 32bit (forced) if you have clients on pre-1910 hub. 
+# Author: bpeppin, 2/24/20
+
+@'
+$GUID = (Get-Item -Path "HKLM:SOFTWARE\Microsoft\Provisioning\OMADM\Accounts\*" -ErrorAction SilentlyContinue).PSChildname
+if ($GUID -eq $null)
+{
+	return "No_MDM_GUID"
+}
+	
+	$path2 = "HKLM:\SOFTWARE\Microsoft\Enrollments\$guid"
+	$upn = (Get-ItemProperty -Path $PATH2 -ErrorAction SilentlyContinue).UPN
+	$enrollmentState = (Get-ItemProperty -Path $PATH2 -ErrorAction SilentlyContinue).EnrollmentState
+	$providerID = (Get-ItemProperty -Path $PATH2 -ErrorAction SilentlyContinue).ProviderID
+
+	if ($providerID -eq "AirWatchMDM" -and $enrollmentState -eq 1)
+	{
+		[string]$EnrollmentSID = (Get-ChildItem HKLM:\SOFTWARE\Microsoft\EnterpriseResourceManager\Tracked\$guid | Where-Object { $_.Name -notlike "*device" }).PSChildName
+		
+		If ($EnrollmentSID)
+		{
+			return $EnrollmentSID
+		}
+		else
+		{
+			Return "No_Enrollment_SID_Found"
+		}
+	}
+	else
+	{
+		return "No_MDM_Enrollment"
+	}
+ 
+'@ | &"$env:windir\Sysnative\WindowsPowerShell\v1.0\powershell.exe" -Command -
+
+# bpeppin, Updated 2/24/20. 
+# Querying which Windows SID has MDM Enrollment
+# Return Type: String
+# Execution Context: Auto (this requires 1910 agent or newer since it needs 64bit powershell) 
+# Author: bpeppin, 2/24/20
+
+$GUID = (Get-Item -Path "HKLM:SOFTWARE\Microsoft\Provisioning\OMADM\Accounts\*" -ErrorAction SilentlyContinue).PSChildname
+if ($GUID -eq $null)
+{
+	return "No_MDM_GUID"
+}
+	
+$path2 = "HKLM:\SOFTWARE\Microsoft\Enrollments\$guid"
+$upn = (Get-ItemProperty -Path $PATH2 -ErrorAction SilentlyContinue).UPN
+$enrollmentState = (Get-ItemProperty -Path $PATH2 -ErrorAction SilentlyContinue).EnrollmentState
+$providerID = (Get-ItemProperty -Path $PATH2 -ErrorAction SilentlyContinue).ProviderID
+
+if ($providerID -eq "AirWatchMDM" -and $enrollmentState -eq 1)
+{
+	[string]$EnrollmentSID = (Get-ChildItem HKLM:\SOFTWARE\Microsoft\EnterpriseResourceManager\Tracked\$guid | Where-Object { $_.Name -notlike "*device" }).PSChildName
+	
+	If ($EnrollmentSID)
+	{
+		return $EnrollmentSID
+	}
+	else
+	{
+		Return "No_Enrollment_SID_Found"
+	}
+}
+else
+{
+	return "No_MDM_Enrollment"
+}
---
+++ Windows-Samples/Sensors/get_windows_sid.ps1
@@ -0,0 +1,12 @@
+﻿# Queries the current logged in user windows SID from HKU. 
+# Return Type: String
+# Execution Context: System
+# Execution Architecture: Auto
+# Author: bpeppin, 2/24/20
+
+New-PSDrive HKU Registry HKEY_USERS | out-null
+$SID = (get-childitem HKU: | Where-Object { $_.Name -like "*S-1-12-1*" -or $_.Name -like "*S-1-5-21*" -And $_.Name -notlike "*_classes" }).Name
+$SID = $SID.Split('\')[1]
+Remove-PSDrive HKU
+
+return $SID
