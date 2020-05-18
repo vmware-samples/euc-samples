90750aea6622a090c02facb804b5b2ef94764e34
Thiago Valcesia <33132852+valcesia@users.noreply.github.com>
2020-05-18 11:07:56 -0300

    Sensors - Get IPv4 and IPv6

---
+++ Windows-Samples/Sensors/os_ipv4_address.ps1
@@ -0,0 +1,7 @@
+﻿# Returns the system's IPv4 Address
+# Return Type: String
+# Execution Context: User
+# Author: tvalcesia
+
+$IPv4 = (Get-WmiObject win32_Networkadapterconfiguration | Where-Object{$_.ipaddress -notlike $null}).IPaddress | Select-Object -First 1
+Write-Output $IPv4 
---
+++ Windows-Samples/Sensors/os_ipv6_address.ps1
@@ -0,0 +1,7 @@
+﻿# Returns the system's IPv6 Address
+# Return Type: String
+# Execution Context: User
+# Author: tvalcesia
+
+$IPv6 = (Get-WmiObject win32_Networkadapterconfiguration | Where-Object{$_.ipaddress -notlike $null}).IPaddress[3]
+Write-Output $IPv6 
