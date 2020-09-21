e31a97b191d62e7b6f06c19abbc2e8dac210b229
Robert Terakedis <rterakedis@vmware.com>
2020-09-20 23:10:55 -0400

    Submitting Cherry-Picked Files from PR:120.

---
+++ Windows-Samples/Sensors/os_ipv4_address.ps1
@@ -0,0 +1,8 @@
+﻿# Returns the system's IPv4 Address
+# Return Type: String
+# Execution Context: User
+# Author: tvalcesia
+# Cherry-Picked from PR:   https://github.com/vmware-samples/euc-samples/pull/120
+
+$IPv4 = (Get-WmiObject win32_Networkadapterconfiguration | Where-Object{$_.ipaddress -notlike $null}).IPaddress | Select-Object -First 1
+Write-Output $IPv4 
