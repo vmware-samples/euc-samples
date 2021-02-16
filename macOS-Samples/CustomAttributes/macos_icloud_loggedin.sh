9363e10fe9736396db50c869bd00c85a2d8d2323
Robert Terakedis <rterakedis@vmware.com>
2021-02-16 11:55:22 -0500

    iCloud Custom Attributes

---
+++ macOS-Samples/CustomAttributes/macos_icloud_loggedin.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+loggedInUser=$( echo "show State:/Users/ConsoleUser" | /usr/sbin/scutil | /usr/bin/awk '/Name :/ && ! /loginwindow/ { print $3 }' )
+if [ $(/usr/libexec/PlistBuddy -c "Print ::Accounts:0:LoggedIn" /Users/$loggedInUser/Library/Preferences/MobileMeAccounts.plist) ]; then
+    LOGGEDIN="TRUE"
+else
+    LOGGEDIN="FALSE"
+fi
+echo $LOGGEDIN
