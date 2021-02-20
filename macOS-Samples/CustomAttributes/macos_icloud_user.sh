aa3904965d656458957c7e11d858a57ae173fdda
Robert Terakedis <rterakedis@vmware.com>
2021-02-19 23:51:17 -0500

    Update to Scripts
    Signed-off-by: Robert Terakedis <rterakedis@vmware.com>

--- macOS-Samples/CustomAttributes/macos_icloud_user.sh
+++ macOS-Samples/CustomAttributes/macos_icloud_user.sh
@@ -1,9 +1,19 @@
 #!/bin/zsh
+
 loggedInUser=$( echo "show State:/Users/ConsoleUser" | /usr/sbin/scutil | /usr/bin/awk '/Name :/ && ! /loginwindow/ { print $3 }' )
-LOGGEDIN=$(/usr/libexec/PlistBuddy -c "Print ::Accounts:0:AccountID" /Users/$loggedInUser/Library/Preferences/MobileMeAccounts.plist)
-RESULT=$?
-if [ $RESULT -eq 0 ]; then
-    echo $LOGGEDIN
+
+if [ -f /Users/$loggedInUser/Library/Preferences/MobileMeAccounts.plist ]; then
+
+    LOGGEDIN=$(/usr/libexec/PlistBuddy -c "Print ::Accounts:0:AccountID" /Users/$loggedInUser/Library/Preferences/MobileMeAccounts.plist)
+    RESULT=$?
+    if [ $RESULT -eq 0 ]; then
+        echo $LOGGEDIN
+    else
+        echo "none"
+    fi
+
 else
+
     echo "none"
+
 fi
