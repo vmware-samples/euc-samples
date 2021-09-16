df359bff758f5887a3af33ee0c18ef9ca33b6d31
helmlingp <66004946+helmlingp@users.noreply.github.com>
2021-09-17 08:06:53 +0930

    Update win11_get_tpm_version.ps1
    resolved error when no TPM detected

--- Windows-Samples/Sensors/win11_get_tpm_version.ps1
+++ Windows-Samples/Sensors/win11_get_tpm_version.ps1
@@ -5,14 +5,13 @@
 # Return Type: String
 # Execution Context: System 
 # Execution Architecture: Auto
-
+$NameSpace= "root\cimv2\security\microsofttpm"
 $tpm = Get-WmiObject -Namespace $NameSpace -Query "Select * from win32_tpm"
-if($tpm.SpecVersion.Contains("2.0")){
-    return "Windows 11 Ready"}
-else{
-    if($tpm.SpecVersion.Contains("1.2")){
-        return "Windows 11 OK"}
-    else{
-        return "NOT Windows 11 Ready"
+if($tpm) {
+    if($tpm.SpecVersion.Contains("2.0")){
+        return "Windows 11 Ready"}
+    else {
+        if($tpm.SpecVersion.Contains("1.2")){
+            return "Windows 11 OK"}
     }
-}
+} else {return "NOT Windows 11 Ready"}
