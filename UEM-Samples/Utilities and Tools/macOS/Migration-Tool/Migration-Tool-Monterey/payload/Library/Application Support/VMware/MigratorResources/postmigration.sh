0bc3b27d710b36384caec1ebdfcddccb0b583efb
Matt Zaske <63124926+mzaske3@users.noreply.github.com>
2022-08-05 15:20:18 -0400

    Delete UEM-Samples/Utilities and Tools/macOS/Migration-Tool/Migration-Tool-Monterey directory

--- UEM-Samples/Utilities and Tools/macOS/Migration-Tool/Migration-Tool-Monterey/payload/Library/Application Support/VMware/MigratorResources/postmigration.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-### Use this script to any migration teardown, such as demoting the user back to Standard. ###
-### This script only runs after the new enrollment profile is installed. ###
-
-DEPNOTIFYLOG="/private/var/tmp/depnotify.log"
-
-echo "Status: Running Post-Migration script" >> $DEPNOTIFYLOG
-
-exit 0
