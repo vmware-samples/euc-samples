cc04af92c7d2400a7b4eb6e4b3fe78fd66e5206c
Leon Letto <lettol@vmware.com>
2022-10-04 10:04:07 -0700

    Updated sensor so it uses python3 included with Workspace ONE and does not need to create files.

--- UEM-Samples/Sensors/macOS/Custom Attributes/active_ip_address.sh
+++ UEM-Samples/Sensors/macOS/Custom Attributes/active_ip_address.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-/usr/bin/touch /tmp/ip.py
-echo """#!/usr/bin/python
-
+/Library/Application\ Support/AirWatch/Data/Munki/bin/Python.framework/Versions/3.10/bin/python3 <<- "EOF"
 import subprocess
 
-active_interface = subprocess.check_output(['/sbin/route', 'get', 'www.apple.com'])
+active_interface = subprocess.check_output(['/sbin/route', 'get', 'www.apple.com']).decode('utf8')
 interface = active_interface.find('interface')
 adapter = active_interface[(int(interface) + 11):].splitlines()[0]
 
-ifconfig = subprocess.check_output(['/sbin/ifconfig', str(adapter)])
+ifconfig = subprocess.check_output(['/sbin/ifconfig', str(adapter)]).decode('utf8')
 inet = ifconfig.find('inet ')
 ip = ifconfig[(int(inet) + 5):].split(' ')[0]
 
-print ip""" >> /tmp/ip.py
-
-/bin/chmod +x /tmp/ip.py
-
-/usr/bin/python /tmp/ip.py
-
-/bin/rm -rf /tmp/ip.py
+print(ip)
+EOF
