3b91636ee6572860675cfa86f68b408226a351e7
Phil Helmling <helmlingp@vmware.com>
2023-02-14 14:29:41 +1030

    Add DeleteSCCMWSUSKeys script

---
+++ UEM-Samples/Scripts/Windows/DeleteSCCMWSUSKeys.ps1
@@ -0,0 +1,9 @@
+# Description: Deletes the SCCM WSUS registry keys that prevent a Windows 10 machine from using a modern managed Windows Update Profile. 
+# Once deleted the machine will honour the WS1 delivered WUS or WSUS configuration settings.
+# Execution Context: SYSTEM
+# Execution Architecture: EITHER_64BIT_OR_32BIT
+# Timeout: 30
+# Variables: RegPath,"HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate"; Regkey1,"WUServer"; Regkey2,"WUStatusServer"
+
+Remove-ItemProperty -Path $env:RegPath -Name $env:Regkey1
+Remove-ItemProperty -Path $env:RegPath -Name $env:Regkey2
