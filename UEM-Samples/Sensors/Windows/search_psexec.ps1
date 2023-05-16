0e59feb801d09a02424a62d98a480113023671d8
Phil Helmling <helmlingp@vmware.com>
2023-05-17 08:09:28 +0930

    updated search_psexec.ps1 sensor

--- UEM-Samples/Sensors/Windows/search_psexec.ps1
+++ UEM-Samples/Sensors/Windows/search_psexec.ps1
@@ -1,7 +1,7 @@
-# Searches file system for existence of psexec.exe and psexec64.exe on C drive (ensure you test to ensure it runs and completes in a timely matter!)
-# Return Type: String
-# Execution Context: System
-# Author: bpeppin
+# Description: Returns list of psexec.exe and psexec64.exe files on C drive
+# Execution Context: SYSTEM
+# Execution Architecture: EITHER64OR32BIT
+# Return Type: STRING
 
 $psexec = Get-Childitem -Path C:\ -Include psexec.exe,psexec64.exe -Recurse -ErrorAction SilentlyContinue
 
@@ -16,21 +16,3 @@ for ($i=0; $i -lt $path.length; $i++)
 Return $list
 }
 
-
-# Searches file system for existence of psexec.exe and psexec64.exe on in users folder only (less impact on disk)
-# Return Type: String
-# Execution Context: User
-# Author: bpeppin
-
-$psexec = Get-Childitem -Path C:\Users -Include psexec.exe,psexec64.exe -Recurse -ErrorAction SilentlyContinue
-
-If ($psexec)
-{
-$path = $psexec.FullName
-
-for ($i=0; $i -lt $path.length; $i++)
-    {
-	$list += $path[$i] + ","
-    }
-Return $list
-}
